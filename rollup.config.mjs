b0daa81891263e58306f0146c23491ac42957dbd
stephenasuncionDEV <stephenasuncion02@gmail.com>
2023-12-01 18:40:45 -0800

    v0.0.2

--- rollup.config.mjs
+++ rollup.config.mjs
@@ -1,6 +1,7 @@
 import terser from "@rollup/plugin-terser";
 import { nodeResolve } from "@rollup/plugin-node-resolve";
 import commonjs from "@rollup/plugin-commonjs";
+import builtins from 'rollup-plugin-node-builtins';
 
 export default {
   input: "src/index.js",
@@ -8,5 +9,5 @@ export default {
     file: "cdn/bundle.js",
     format: "cjs",
   },
-  plugins: [nodeResolve({ browser: true }), commonjs(), terser()],
+  plugins: [nodeResolve({ browser: true }), commonjs(), builtins(), terser()],
 };
